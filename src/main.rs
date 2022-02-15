075e278f298df554a9766f7fe9bde37c5a56f1e7
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-02-15 19:42:12 -0300

    feat: improve logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::time::Duration;
 
-use dotenv::dotenv;
-use tokio::task::JoinHandle;
 use tonic::transport::Server;
 use tracing::info;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
 
 use crate::raft::{Config, Raft};
 
@@ -15,12 +16,22 @@ mod tests;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   std::env::set_var(
     "RUST_LOG",
-    std::env::var("RUST_LOG").unwrap_or_else(|_| String::from("raft=trace")),
+    std::env::var("RUST_LOG").unwrap_or_else(|_| format!("{}=trace", env!("CARGO_PKG_NAME"))),
   );
 
-  dotenv().ok();
+  dotenv::dotenv().ok();
 
-  tracing_subscriber::fmt::init();
+  let (non_blocking_writer, _guard) = tracing_appender::non_blocking(std::io::stdout());
+
+  let app_name = concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION")).to_string();
+
+  let bunyan_formatting_layer = BunyanFormattingLayer::new(app_name, non_blocking_writer);
+
+  let subscriber = Registry::default()
+    .with(JsonStorageLayer)
+    .with(bunyan_formatting_layer);
+
+  tracing::subscriber::set_global_default(subscriber).unwrap();
 
   // ADDRESS=127.0.0.1:5000 PEERS=http://127.0.0.1:5001,http://127.0.0.1:5002 cargo r
   // ADDRESS=127.0.0.1:5001 PEERS=http://127.0.0.1:5000,http://127.0.0.1:5002 cargo r
