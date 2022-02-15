fe2fbfe18712dfe495e7667f3316483296e653d7
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-02-15 19:37:22 -0300

    refactor: server start up

--- src/main.rs
+++ src/main.rs
@@ -26,80 +26,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // ADDRESS=127.0.0.1:5001 PEERS=http://127.0.0.1:5000,http://127.0.0.1:5002 cargo r
   // ADDRESS=127.0.0.1:5002 PEERS=http://127.0.0.1:5000,http://127.0.0.1:5001 cargo r
 
-  start_raft_server(
-    std::env::var("ADDRESS").unwrap(),
-    std::env::var("PEERS")
-      .unwrap()
-      .split(",")
-      .map(|s| s.to_owned())
-      .collect(),
-  )
-  .await?;
-  /*
-  let _result = tokio::join!(
-    start_raft_server(
-      String::from("127.0.0.1:5000"),
-      vec![
-        String::from("http://127.0.0.1:5001"),
-        // String::from("http://127.0.0.1:5002"),
-        // String::from("http://127.0.0.1:5003"),
-        // String::from("http://127.0.0.1:5004)"),
-      ]
-    ),
-    start_raft_server(
-      String::from("127.0.0.1:5001"),
-      vec![
-        String::from("http://127.0.0.1:5000"),
-        // String::from("http://127.0.0.1:5002"),
-        // String::from("http://127.0.0.1:5003"),
-        // String::from("http://127.0.0.1:5004)"),
-      ]
-    ),
-    /*   start_raft_server(
-      String::from("127.0.0.1:5002"),
-      vec![
-        String::from("http://127.0.0.1:5001"),
-        String::from("http://127.0.0.1:5000"),
-        String::from("http://127.0.0.1:5003"),
-        String::from("http://127.0.0.1:5004)"),
-      ]
-    ),
-    start_raft_server(
-      String::from("127.0.0.1:5003"),
-      vec![
-        String::from("http://127.0.0.1:5001"),
-        String::from("http://127.0.0.1:5002"),
-        String::from("http://127.0.0.1:5000"),
-        String::from("http://127.0.0.1:5004)"),
-      ]
-    ),
-    start_raft_server(
-      String::from("127.0.0.1:5004"),
-      vec![
-        String::from("http://127.0.0.1:5001"),
-        String::from("http://127.0.0.1:5002"),
-        String::from("http://127.0.0.1:5003"),
-        String::from("http://127.0.0.1:5000)"),
-      ]
-    )*/
-  );*/
+  let address = std::env::var("ADDRESS").unwrap().parse()?;
+  let peers = std::env::var("PEERS")
+    .unwrap()
+    .split(",")
+    .map(|s| s.to_owned())
+    .collect();
+
+  info!(?address, ?peers, "starting server");
+
+  Server::builder()
+    .add_service(raft::raft_server::RaftServer::new(Raft::new(Config {
+      min_heartbeat_timeout: Duration::from_millis(2000),
+      min_election_timeout: Duration::from_millis(1500),
+      send_heartbeat_timeout: Duration::from_millis(1000),
+      servers: peers,
+    })))
+    .serve(address)
+    .await?;
 
   Ok(())
-}
-
-fn start_raft_server(address: String, servers: Vec<String>) -> JoinHandle<()> {
-  info!("starting server: {}", &address);
-
-  tokio::spawn(async move {
-    Server::builder()
-      .add_service(raft::raft_server::RaftServer::new(Raft::new(Config {
-        min_heartbeat_timeout: Duration::from_millis(2000),
-        min_election_timeout: Duration::from_millis(1500),
-        send_heartbeat_timeout: Duration::from_millis(1000),
-        servers,
-      })))
-      .serve(address.parse().unwrap())
-      .await
-      .unwrap();
-  })
 }
